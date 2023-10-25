f9d8e6b3cf2fc51720ea274d49b542915bf6874c
Bohdan <sqwfunny@gmail.com>
2023-10-25 20:29:23 +0200

    commit

--- AI/start.py
+++ AI/start.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import numpy as np
@@ -6,54 +7,63 @@ import numpy as np
 from keras.layers import Dense, SimpleRNN, Input, Embedding
 from keras.models import Sequential
 from keras.preprocessing.text import Tokenizer
-from keras.utils import to_categorical
 
-with open('text.txt', 'w', encoding='utf-8') as f:
+with open('text.txt', 'r', encoding='utf-8') as f:
     texts = f.read()
     texts = texts.replace('\ufeff', '')
 
 maxWordsCount = 1000
-tokenizer = Tokenizer(num_words=maxWordsCount, filters='!-"@#$%^&*()',
-                      lower=True, split='', char_level=False)
+tokenizer = Tokenizer(num_words=maxWordsCount, filters='!–"—#$%&()*+-/:;<=>?@[\\]^_`{|}~\t\n\r«»',
+                      lower=True, split=' ', char_level=False)
 tokenizer.fit_on_texts([texts])
 
 dist = list(tokenizer.word_counts.items())
+print(dist[:10])
 
 data = tokenizer.texts_to_sequences([texts])
-res = np.array(data[0])
+res = data[0]
 
 inp_words = 3
-n = res.shape[0] - inp_words
+n = len(res) - inp_words
 
-X = np.array([res[i:i + inp_words]for i in range(n)])
-Y = to_categorical(res[inp_words:], num_classes=maxWordsCount)
+X = np.array([res[i:i + inp_words] for i in range(n)])
+Y = np.array(res[inp_words:])
 
 model = Sequential()
-model.add(Embedding(maxWordsCount, 256, input_length = inp_words))
-model.add(SimpleRNN(128,activation='tanh'))
-model.add(Dense(maxWordsCount, activation='sodtmax'))
+model.add(Embedding(maxWordsCount, 256, input_length=inp_words))
+model.add(SimpleRNN(128, activation='tanh', return_sequences=True))
+model.add(SimpleRNN(64, activation='tanh'))
+model.add(Dense(maxWordsCount, activation='softmax'))
 model.summary()
 
-model.compile(loss='categoical_crossentropy', metrics=['accuracy'], optimizer='adam')
+model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
 
-history = model.fit(X, Y, batch_size=32, epochs=50)
+history = model.fit(X, Y, batch_size=32, epochs=150)
 
 
-def buildPhrase(texts, str_len=20):
+def buildPhrase(texts, max_len=150):
     res = texts
-    data = tokenizer.texts_to_sequences([texts])[0]
-    for i in range(str_len):
-        x = data[i: i + inp_words]
-        inp = np.expand_dims(x, axis=0)
+    token_list = tokenizer.texts_to_sequences([texts])[0]
+    while len(res.split()) < max_len:
+        if len(token_list) < inp_words:
+            break
+        context = np.array(token_list[-inp_words:])
+        context = np.expand_dims(context, axis=0)
+        predicted_word_index = np.argmax(model.predict(context))
+        predicted_word = tokenizer.index_word[predicted_word_index]
+        if "dowt." in predicted_word:
+            res += " " + predicted_word
+            break
+        res += " " + predicted_word
+        token_list.append(predicted_word_index)
+    sentences = res.split('. ')
+    sentences_capitalized = [sentence.capitalize() for sentence in sentences]
+    text_capitalized = '. '.join(sentences_capitalized)
+    output = text_capitalized.replace("dowt.", "Dowt.")
+    return output
 
-        pred = model.predict(inp)
-        indx = pred.argmax(axis=1)[0]
-        data.append(indx)
 
-        res += "" + tokenizer.index_word[indx]
-
-    return res
-
-
-res = buildPhrase("позитив добавляет годы")
+res = buildPhrase("Привет. Рад, что ты обратился ко мне за советом. По данным за последний месяц, "
+                  "твой доход составил 4000 долларов, а расходы - 3500 долларов, "
+                  "включая 3000 долларов на регулярные платежи и 500 долларов на накопления.")
 print(res)
