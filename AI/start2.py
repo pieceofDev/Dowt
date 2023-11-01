8b550e8e2361c333aa12fc667774b82e3e71b164
Bohdan <sqwfunny@gmail.com>
2023-11-01 19:01:46 +0100

    commit

--- AI/start2.py
+++ AI/start2.py
@@ -4,22 +4,28 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import numpy as np
 
-from keras.layers import Dense, SimpleRNN, Input, Embedding
+from keras.layers import Dense, Embedding
 from keras.models import Sequential
 from keras.preprocessing.text import Tokenizer
+from keras.src.layers import LSTM, Bidirectional
 
+# Открытие файла с использованием конструкции with для автоматического закрытия файла после завершения операций
 with open('text.txt', 'r', encoding='utf-8') as f:
     texts = f.read()
-    texts = texts.replace('\ufeff', '')
+    texts = texts.replace('\ufeff', '')  # Удаление ненужных символов
 
-maxWordsCount = 1000
+maxWordsCount = 5000
+
+# Обновленная конфигурация токенизатора
 tokenizer = Tokenizer(num_words=maxWordsCount, filters='!–"—#$%&()*+-/:;<=>?@[\\]^_`{|}~\t\n\r«»',
                       lower=True, split=' ', char_level=False)
 tokenizer.fit_on_texts([texts])
 
+# Вывод небольшого отрывка из списка слов
 dist = list(tokenizer.word_counts.items())
 print(dist[:10])
 
+# Преобразование текста в последовательность чисел
 data = tokenizer.texts_to_sequences([texts])
 res = data[0]
 
@@ -31,9 +37,9 @@ Y = np.array(res[inp_words:])
 
 model = Sequential()
 model.add(Embedding(maxWordsCount, 256, input_length=inp_words))
-model.add(SimpleRNN(128, activation='tanh', return_sequences=True))
-model.add(SimpleRNN(64, activation='tanh'))
-model.add(Dense(maxWordsCount, activation='softmax'))
+model.add(LSTM(128, activation='tanh', return_sequences=True))
+model.add(Bidirectional(LSTM(64, activation='tanh')))  # Добавление слоя Bidirectional LSTM
+model.add(Dense(2, activation='softmax'))
 model.summary()
 
 model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
@@ -64,6 +70,6 @@ def buildPhrase(texts, max_len=150):
 
 
 res = buildPhrase("Привет. Рад, что ты обратился ко мне за советом. По данным за последний месяц, "
-                  "твой доход составил 10000 долларов, а расходы- 6000 долларов, "
-                  "включая 500 долларов на регулярные платежи и 1000 долларов на накопления.")
+                  "твой доход составил 3000 долларов, а расходы - 3000 долларов, "
+                  "включая 2000 долларов на регулярные платежи и 1000 долларов на накопления.")
 print(res)
