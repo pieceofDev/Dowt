c22d32de6251ac7b042d733df43768d297507787
Serhii Kyselov <tarfash.noko@gmail.com>
2023-11-08 15:07:53 +0100

    added script for text editing if it is needed

---
+++ utils/Text edit script.py
@@ -0,0 +1,13 @@
+list_examples = []
+
+with open("../AI/text.txt", "r") as file:
+    list_examples = file.readlines()
+
+
+for line in range(len(list_examples)):
+    new_str = list_examples[line][:282].replace("Of the", "Of that") + list_examples[line][282:]
+    list_examples[line] = (new_str)
+
+with open("../AI/text.txt", "w") as file:
+    for line in list_examples:
+        file.write(line)
