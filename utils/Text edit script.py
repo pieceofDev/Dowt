733e078e17c139fb0f110fd049ad242045da8389
Serhii Kyselov <tarfash.noko@gmail.com>
2023-11-08 15:14:07 +0100

    edited script, some stylistics wrongs

--- utils/Text edit script.py
+++ utils/Text edit script.py
@@ -1,13 +1,10 @@
-list_examples = []
-
-with open("../AI/text.txt", "r") as file:
+with open("AI/text.txt") as file:
     list_examples = file.readlines()
-
 
 for line in range(len(list_examples)):
     new_str = list_examples[line][:282].replace("Of the", "Of that") + list_examples[line][282:]
-    list_examples[line] = (new_str)
+    list_examples[line] = new_str
 
-with open("../AI/text.txt", "w") as file:
+with open("AI/text.txt", "w") as file:
     for line in list_examples:
         file.write(line)
